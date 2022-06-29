6d50997976a12d448dba676c6fcf5958fbc8db00
Shankarbojanki <108254226+Shankarbojanki@users.noreply.github.com>
2022-06-29 11:18:52 +0530

    Chages allowed

--- src/Newpackage/Idontknow.java
+++ src/Newpackage/Idontknow.java
@@ -2,6 +2,6 @@ package Newpackage;
 
 public class Idontknow {
     public static void main(String[] args) {
-        System.out.println("ela add ceyyali new package");
+        System.out.println("Hello world");
     }
 }
